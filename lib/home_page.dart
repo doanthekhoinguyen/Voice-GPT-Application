d95f52ecd0936204c7c236ecee96b5fa43f11614
doanthekhoinguyen <doanthekhoinguyen.dev@gmail.com>
2023-06-14 09:40:51 +0700

    2 complete UI

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:assistant/feature_box.dart';
 import 'package:assistant/pallete.dart';
 import 'package:flutter/material.dart';
 
@@ -24,7 +25,7 @@ class _HomePageState extends State<HomePage> {
             children: [
               Center(
                 child: Container(
-                  height: 120,
+                  height: 90,
                   width: 120,
                   margin: const EdgeInsets.only(top: 4),
                   decoration: const BoxDecoration(
@@ -33,7 +34,7 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
               Container(
-                height: 125,
+                height: 100,
                 decoration: const BoxDecoration(
                   shape: BoxShape.circle,
                   image: DecorationImage(
@@ -52,14 +53,73 @@ class _HomePageState extends State<HomePage> {
               vertical: 10,
             ),
             margin: const EdgeInsets.symmetric(horizontal: 40).copyWith(
-              top: 30,
+              top: 15,
             ),
             decoration: BoxDecoration(
                 border: Border.all(
-              color: Pallete.borderColor,
-            )),
+                  color: Pallete.borderColor,
+                ),
+                borderRadius: BorderRadius.circular(20).copyWith(
+                  topLeft: Radius.zero,
+                )),
+            child: const Padding(
+              padding: EdgeInsets.symmetric(vertical: 5.0),
+              child: Text(
+                'Hi Khoi Nguyen, what task can I do for you?',
+                style: TextStyle(
+                  fontFamily: 'Cera Pro',
+                  color: Pallete.mainFontColor,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(5),
+            alignment: Alignment.centerLeft,
+            margin: const EdgeInsets.only(
+              top: 10,
+              left: 22,
+            ),
+            child: const Text(
+              'Here are a few feature',
+              style: TextStyle(
+                fontFamily: 'Cere Pro',
+                color: Pallete.mainFontColor,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          //featrue list
+          const Column(
+            children: [
+              FeatureBox(
+                color: Pallete.firstSuggestionBoxColor,
+                headerText: "Chat GPT",
+                descriptionText:
+                    'A smarter way to organized and informed with Chat GPT',
+              ),
+              FeatureBox(
+                color: Pallete.secondSuggestionBoxColor,
+                headerText: "Dall-E",
+                descriptionText:
+                    'Get inspired and stay creative with personal powered by Dall-E',
+              ),
+              FeatureBox(
+                color: Pallete.thirdSuggestionBoxColor,
+                headerText: "Smart Voice",
+                descriptionText:
+                    'Get the best of both worlds with a voice assistant powered',
+              ),
+            ],
           ),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Pallete.firstSuggestionBoxColor,
+        onPressed: () {},
+        child: const Icon(Icons.mic),
       ),
     );
   }
