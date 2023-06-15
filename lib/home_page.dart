f3b6cfe512e5eae1a4c9ac87d0650ea95413a0e2
doanthekhoinguyen <doanthekhoinguyen.dev@gmail.com>
2023-06-15 11:39:07 +0700

    3 speech to text

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:assistant/feature_box.dart';
 import 'package:assistant/pallete.dart';
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +12,50 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final speechToText = SpeechToText();
+  String lastWords = '';
+
+  @override
+  void initState() {
+    super.initState();
+    initSpeechToText();
+  }
+
+  Future<void> initSpeechToText() async {
+    await speechToText.initialize();
+    setState(() {});
+  }
+
+  /// Each time to start a speech recognition session
+  Future<void> startListening() async {
+    await speechToText.listen(onResult: onSpeechResult);
+    setState(() {});
+  }
+
+  /// Manually stop the active speech recognition session
+  /// Note that there are also timeouts that each platform enforces
+  /// and the SpeechToText plugin supports setting timeouts on the
+  /// listen method.
+  Future<void> stopListening() async {
+    await speechToText.stop();
+    setState(() {});
+  }
+
+  /// This is the callback that the SpeechToText plugin calls when
+  /// the platform returns recognized words.
+  void onSpeechResult(SpeechRecognitionResult result) {
+    setState(() {
+      lastWords = result.recognizedWords;
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    speechToText.stop();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,107 +64,117 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         leading: const Icon(Icons.menu),
       ),
-      body: Column(
-        children: [
-          // image virtual assitant
-          Stack(
-            children: [
-              Center(
-                child: Container(
-                  height: 90,
-                  width: 120,
-                  margin: const EdgeInsets.only(top: 4),
-                  decoration: const BoxDecoration(
-                      color: Pallete.assistantCircleColor,
-                      shape: BoxShape.circle),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // image virtual assitant
+            Stack(
+              children: [
+                Center(
+                  child: Container(
+                    height: 90,
+                    width: 120,
+                    margin: const EdgeInsets.only(top: 4),
+                    decoration: const BoxDecoration(
+                        color: Pallete.assistantCircleColor,
+                        shape: BoxShape.circle),
+                  ),
                 ),
-              ),
-              Container(
-                height: 100,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    image: AssetImage(
-                      'assets/images/virtualAssistant.png',
+                Container(
+                  height: 100,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: AssetImage(
+                        'assets/images/virtualAssistant.png',
+                      ),
                     ),
                   ),
                 ),
+              ],
+            ),
+            //chat bubble
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 10,
               ),
-            ],
-          ),
-          //chat bubble
-          Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 10,
-            ),
-            margin: const EdgeInsets.symmetric(horizontal: 40).copyWith(
-              top: 15,
-            ),
-            decoration: BoxDecoration(
-                border: Border.all(
-                  color: Pallete.borderColor,
+              margin: const EdgeInsets.symmetric(horizontal: 40).copyWith(
+                top: 15,
+              ),
+              decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Pallete.borderColor,
+                  ),
+                  borderRadius: BorderRadius.circular(20).copyWith(
+                    topLeft: Radius.zero,
+                  )),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(vertical: 5.0),
+                child: Text(
+                  'Hi Khoi Nguyen, what task can I do for you?',
+                  style: TextStyle(
+                    fontFamily: 'Cera Pro',
+                    color: Pallete.mainFontColor,
+                    fontSize: 20,
+                  ),
                 ),
-                borderRadius: BorderRadius.circular(20).copyWith(
-                  topLeft: Radius.zero,
-                )),
-            child: const Padding(
-              padding: EdgeInsets.symmetric(vertical: 5.0),
-              child: Text(
-                'Hi Khoi Nguyen, what task can I do for you?',
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(5),
+              alignment: Alignment.centerLeft,
+              margin: const EdgeInsets.only(
+                top: 10,
+                left: 22,
+              ),
+              child: const Text(
+                'Here are a few feature',
                 style: TextStyle(
-                  fontFamily: 'Cera Pro',
+                  fontFamily: 'Cere Pro',
                   color: Pallete.mainFontColor,
                   fontSize: 20,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
             ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(5),
-            alignment: Alignment.centerLeft,
-            margin: const EdgeInsets.only(
-              top: 10,
-              left: 22,
+            //featrue list
+            const Column(
+              children: [
+                FeatureBox(
+                  color: Pallete.firstSuggestionBoxColor,
+                  headerText: "Chat GPT",
+                  descriptionText:
+                      'A smarter way to organized and informed with Chat GPT',
+                ),
+                FeatureBox(
+                  color: Pallete.secondSuggestionBoxColor,
+                  headerText: "Dall-E",
+                  descriptionText:
+                      'Get inspired and stay creative with personal powered by Dall-E',
+                ),
+                FeatureBox(
+                  color: Pallete.thirdSuggestionBoxColor,
+                  headerText: "Smart Voice",
+                  descriptionText:
+                      'Get the best of both worlds with a voice assistant powered',
+                ),
+              ],
             ),
-            child: const Text(
-              'Here are a few feature',
-              style: TextStyle(
-                fontFamily: 'Cere Pro',
-                color: Pallete.mainFontColor,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          //featrue list
-          const Column(
-            children: [
-              FeatureBox(
-                color: Pallete.firstSuggestionBoxColor,
-                headerText: "Chat GPT",
-                descriptionText:
-                    'A smarter way to organized and informed with Chat GPT',
-              ),
-              FeatureBox(
-                color: Pallete.secondSuggestionBoxColor,
-                headerText: "Dall-E",
-                descriptionText:
-                    'Get inspired and stay creative with personal powered by Dall-E',
-              ),
-              FeatureBox(
-                color: Pallete.thirdSuggestionBoxColor,
-                headerText: "Smart Voice",
-                descriptionText:
-                    'Get the best of both worlds with a voice assistant powered',
-              ),
-            ],
-          ),
-        ],
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Pallete.firstSuggestionBoxColor,
-        onPressed: () {},
+        onPressed: () async {
+          if (await speechToText.hasPermission && speechToText.isNotListening) {
+            await startListening();
+          } else if (speechToText.isListening) {
+            await stopListening();
+          } else {
+            initSpeechToText();
+          }
+        },
         child: const Icon(Icons.mic),
       ),
     );
